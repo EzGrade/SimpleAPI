d20b725e0ad26226d979a00c452b155215baa5dd
Grade <aborecevskij94@gmail.com>
2024-01-08 13:51:42 +0200

    Updating every file

--- SimpleAPI/settings.py
+++ SimpleAPI/settings.py
@@ -126,6 +126,8 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+print(BASE_DIR)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
